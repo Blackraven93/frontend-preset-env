23dbdcdfa2e45eccb91ddfa9e1c02163f6bb4b8e
Blackraven93 <reblackraven@gmail.com>
2021-11-29 17:06:18 +0900

    webpack basic setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,83 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const path = require('path');
 
+// module.exports = {
+//     mode: 'development',
+//     entry: './src/index.js',
+//     output: {
+//         filename: 'bundle.js',
+//         // 보통 첫 인자를 root경로로 하는데 
+//         // 루트 경로가 아닐 시 루트 경로를 앞에 붙임
+//         path: path.resolve(__dirname, 'dist')
+//     },
+//     module: {
+//         rules: [
+//             {   // css파일을 use의 규칙으로 사용하겠다.
+//                 // 오른쪽 부터 왼쪽 순으로 왜?
+//                 // sass -> css로 변환, 일괄 css loader 적용
+//                 // 만약 inline style을 원한다면 'style-loader' 설정
+//                 test: /\.css$/i,
+//                 use: [{loader: MiniCssExtractPlugin.loader}, 'css-loader']
+//             }
+//         ]
+//     },
+//     // s빼먹지마 ㅠㅠ
+//     plugins: [
+//         new MiniCssExtractPlugin(),
+//         new HtmlWebpackPlugin({
+//             template:'./src/index.html'
+//         })
+//     ],
+//     devServer: {
+//         port:9000,
+//         hot: true,
+//         proxy: {
+//             '/api' : 'domain.com',
+//             changeOrigin: true
+//         }
+//     }
+// };
+
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
+    mode:'development',
+    entry: {
+        raven: './src/index.js',
+        app: ['./src/test/app.js', './src/test/bpp.js']
+    },
     output: {
-        filename: 'bundle.js',
-        // 보통 첫 인자를 root경로로 하는데 
-        // 루트 경로가 아닐 시 루트 경로를 앞에 붙임
-        path: path.resolve(__dirname, 'dist')
+        filename:'[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
     },
     module: {
         rules: [
-            {   // css파일을 use의 규칙으로 사용하겠다.
-                // 오른쪽 부터 왼쪽 순으로 왜?
-                // sass -> css로 변환, 일괄 css loader 적용
-                // 만약 inline style을 원한다면 'style-loader' 설정
-                test: /\.css$/i,
-                use: [{loader: MiniCssExtractPlugin.loader}, 'css-loader']
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        [
+                            '@babel/preset-env', {
+                                targets: {node:'current'}, // 노드일 경우만
+                                modules: 'false',
+                                useBuiltIns: 'usage'
+                            }
+                        ],
+                        // '@babel/preset-react', // 리액트를 사용한다면
+                        // '@babel/preset-typescript' // 타입스크립트를 사용한다면
+                    ]
+                },
+                exclude: ['/node_modules'],
             }
         ]
     },
-    // s빼먹지마 ㅠㅠ
-    plugins: [
-        new MiniCssExtractPlugin(),
-        new HtmlWebpackPlugin({
-            template:'./src/index.html'
-        })
-    ],
-    devServer: {
-        port:9000
-    }
+    plugins:[],
+    optimization: {},
+    resolve: {
+        modules: ['node_modules'],
+        extensions: ['.js', '.json', '.jsx', '.css'],
+    },
 };
+
